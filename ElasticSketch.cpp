20b1335fb97ec21796fa1c2368c938c5d3c391eb
Bruno Arce R <brunoarce0@gmail.com>
2023-09-11 15:08:53 -0300

    Create ElasticSketch.cpp

---
+++ ElasticSketch.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include "CountMin.h"
+#include "CountMinCU.h"
+
+using namespace std;
+
+class tupla4
+{
+public:
+    // int elemento;
+    int v_up;
+    bool flag;
+    int v_down;
+};
+
+int main()
+{
+}
